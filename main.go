e639c9b2f542f267327a8358939d3f96a5a61b49
Mayuri <63649939+opensourcefuture@users.noreply.github.com>
2021-08-22 14:29:07 +0800

    Update main.go

--- main.go
+++ main.go
@@ -81,7 +81,7 @@ func main() {
 		Driver: []zero.Driver{
 			&driver.WSClient{
 				// OneBot 正向WS 默认使用 6700 端口
-				Url:         "ws://127.0.0.1:6700",
+				Url:         "ws://127.0.0.1:6703",
 				AccessToken: "1145141919810",
 			},
 		},
